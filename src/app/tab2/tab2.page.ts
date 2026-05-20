204a5f14a97e37d17770ee9469f9ce4e42b2b7fc
MatheusPortal <matheusgamer714@gmail.com>
2026-05-20 15:15:09 -0300

    Finalização do app

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -5,7 +5,7 @@ import { IonContent,
   IonCardContent,
   IonCardHeader,
   IonCardSubtitle,
-  IonCardTitle, } from '@ionic/angular/standalone';
+  IonCardTitle, IonProgressBar, } from '@ionic/angular/standalone';
 
 import { ServiceFakestore } from '../services/service.fakestore';
 
@@ -14,21 +14,38 @@ import { ServiceFakestore } from '../services/service.fakestore';
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
-  imports: [ IonContent,   IonButton, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, ]
+  imports: [ IonContent, IonButton, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonProgressBar ]
 })
 
 
 export class Tab2Page implements OnInit {
   public fakeStore: any;
   public storeKeys: string[] = [];
-  
+  public cardExibir: number = -1;
+
+
   constructor(private ServiceFakestore: ServiceFakestore) {}
 
   ngOnInit(): void {
     this.ServiceFakestore.getFakeStore().subscribe(
-      (data) => {this.fakeStore = data; this.storeKeys = Object.keys(data); console.log(data)}
+      (data) => {this.fakeStore = data; this.storeKeys = Object.keys(data); this.cardExibir = 0}
     );
   }
 
+  nextCard() {
+    if (this.cardExibir + 1 > this.storeKeys.length - 1) {
+      this.cardExibir = 0;
+    } else {
+      this.cardExibir += 1;
+    }
+  }
+
+  backCard() {
+    if (this.cardExibir - 1 < 0) {
+      this.cardExibir = this.storeKeys.length - 1;
+    } else {
+      this.cardExibir -= 1;
+    }
+  }
   
 }
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,25 +1,45 @@
 import { Component, OnInit } from '@angular/core';
-import { IonContent } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { IonContent, 
+  IonButton,
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle, IonProgressBar,
+  IonButtons,
+  IonTitle,
+  IonToolbar } from '@ionic/angular/standalone';
 import { ServiceFakestore } from '../services/service.fakestore';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss'],
-  imports: [IonContent ],
+  imports: [IonContent, 
+  IonButton,
+  IonCardContent,
+  IonCardHeader,
+  IonCardSubtitle, IonProgressBar,
+  IonButtons,
+  IonTitle,
+  IonToolbar, IonContent, IonButton, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonProgressBar ],
 })
 
 
 export class Tab3Page implements OnInit {
   public fakeStore: any;
   public storeKeys: string[] = [];
-  
+  public exibirCards: boolean = false;
+
   constructor(private ServiceFakestore: ServiceFakestore) {}
   
   ngOnInit(): void {
     this.ServiceFakestore.getFakeStore().subscribe(
-      (data) => {this.fakeStore = data; this.storeKeys = Object.keys(data); console.log(data)}
+      (data) => {this.fakeStore = data; this.storeKeys = Object.keys(data)}
     );
+  }
+
+  modoExibir(estado: boolean): void {
+    this.exibirCards = estado;
   }
 }
